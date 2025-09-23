c7dd5da180b44ae0664f4f3efdb4d451ebbdb152
Waqqir Humaid <waqqirhumaid1@gmail.com>
2025-09-23 14:12:41 +0700

    feat: menambahkan hero section

--- src/components/CarouselHero.tsx
+++ src/components/CarouselHero.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import {
   Carousel,
@@ -6,25 +7,74 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "./ui/carousel";
+import CountUp from "./CountUp";
 
 export default function CarouselHero() {
   return (
-    <Carousel className="w-full max-w-[400px] bg-black">
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      {/* <CarouselPrevious />
+    <div className="relative z-20 flex justify-center items-center mt-15">
+      <Carousel className=" relative z-20 lg:w-120 md:w-90 sm:w-80 w-80 sm:ml-5">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1 ">
+                <Card>
+                  <CardContent className="flex items-center justify-center w-full lg:h-150 md:h-100 h-auto">
+                    <Image
+                      src={"/rijal.png"}
+                      width={600}
+                      height={600}
+                      alt="Rijal"
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        {/* <CarouselPrevious />
       <CarouselNext /> */}
-    </Carousel>
+        <div className="absolute z-30 flex flex-col justify-center items-center lg:w-35 lg:h-35 md:w-25 md:h-25 sm:w-26 sm:h-26  w-20 h-20 mr-5 bg-white border-3 border-[#67866B] lg:top-20 sm:top-4 top-0 -right-5 rotate-15 rounded-2xl ">
+          <h1 className="text-[#4D6450] font-bold text-xl lg:text-2xl">
+            <CountUp
+              from={0}
+              to={1.419}
+              separator=","
+              direction="up"
+              duration={2}
+              className="count-up-text"
+            />
+          </h1>
+          <h1 className="text-[#4D6450] font-bold lg:text-xl">SISWA</h1>
+        </div>
+        <div className="absolute z-30 lg:w-35 lg:h-35 md:w-25 md:h-25 w-20 sm:w-26 sm:h-26 flex flex-col justify-center items-center h-20 mr-5 bg-white border-3 border-[#67866B] lg:top-64 sm:top-40 top-30 -right-6 -rotate-15 rounded-2xl ">
+          {" "}
+          <h1 className="text-[#4D6450] font-bold text-xl lg:text-2xl">
+            <CountUp
+              from={0}
+              to={56}
+              separator=","
+              direction="up"
+              duration={2}
+              className="count-up-text"
+            />
+          </h1>
+          <h1 className="text-[#4D6450] font-bold lg:text-xl">GURU</h1>
+        </div>
+        <div className="absolute z-30 lg:w-35 lg:h-35 md:w-25 md:h-25 sm:w-26 sm:h-26 w-20 h-20 mr-5 flex flex-col justify-center items-center bg-white border-3 border-[#67866B] lg:top-107 sm:top-80 top-60 -right-7 rotate-10 rounded-2xl ">
+          {" "}
+          <h1 className="text-[#4D6450] font-bold text-xl lg:text-2xl">
+            <CountUp
+              from={0}
+              to={50}
+              separator=","
+              direction="up"
+              duration={2}
+              className="count-up-text"
+            />
+          </h1>
+          <h1 className="text-[#4D6450] font-bold lg:text-xl">Ruang Kelas</h1>
+        </div>
+      </Carousel>
+    </div>
   );
 }
