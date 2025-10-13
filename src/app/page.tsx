d4476334156652cd22b5e36ad10b2acd11976b69
Waqqir Humaid <waqqirhumaid1@gmail.com>
2025-10-13 21:24:06 +0700

    feat: benerin page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-export default function HomePage() {
+import AgendaSection from "@/components/_components/AgendaSection";
+import ArtikelSection from "@/components/_components/ArtikelSection";
+import GaleriSection from "@/components/_components/GaleriSection";
+import HeroSection from "@/components/_components/HeroSection";
+import KepalaSekollah from "@/components/_components/KepalaSekolah";
+import LokasiSection from "@/components/_components/LokasiSection";
+import StaffSekolahSection from "@/components/_components/StaffSekolah";
+
+export default function Home() {
   return (
-    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif', fontSize: '24px' }}>
-      <h1>Tes Berhasil</h1>
-      <p>Server Next.js berjalan dengan baik.</p>
-    </div>
+    <main className="overflow-hidden bg-image" >
+      <HeroSection />
+      <KepalaSekollah/>
+      <StaffSekolahSection/>
+      <GaleriSection/>
+      <ArtikelSection/>
+      <AgendaSection/>
+      <LokasiSection/>
+    </main>
   );
 }
